74e82b5363e6791a63f39daefa9f83b5a7420ab2
Matthijs Openneer <matthijs.openneer@gmail.com>
2019-03-11 10:32:32 +0100

    added cache key and reset

--- src/Analytics.php
+++ src/Analytics.php
@@ -11,12 +11,26 @@ use Cache;
 class Analytics
 {
 
+    public $timezone;
 
     /** @var string */
     protected $viewId;
 
     /** @var string */
     protected $developerKey;
+
+    /** @var string */
+    protected $startDate;
+
+    /** @var string */
+    protected $endDate;
+
+
+    public function __construct()
+    {
+        $this->timezone = config('app.timezone');
+        $this->reset();
+    }
 
 
     public function client($applicationName, $developerKey, $viewId)
@@ -48,6 +62,8 @@ class Analytics
 
         $response = $this->analytics->data_realtime->call('get', [['ids' => "ga:".$this->viewId]+$params], 'Google_Service_Analytics_RealtimeData');
 
+        $this->reset();
+
         return ($response['rows'] ?? []);
     }
 
@@ -60,17 +76,17 @@ class Analytics
      * @param  string $filters    rt:pagePath=~/tests/22/question
      * @return array
      */
-    public function period($metrics = '', $dimensions = '', $filters = '', $cacheTime = null)
+    public function period($metrics = '', $dimensions = '', $filters = '', $from = '', $to = '', $cacheTime = null)
     {
         $params = $this->getParams($metrics, $dimensions, $filters);
-        $endDate = ($to) ? $to : $this->endDate->format('Y-m-d');
-        $startDate = ($from) ? $from : $this->startDate->format('Y-m-d');
+        $this->endDate = ($to) ? $to : $this->endDate->format('Y-m-d');
+        $this->startDate = ($from) ? $from : $this->startDate->format('Y-m-d');
 
-        $result = Cache::remember('key', 0, function() use ($metrics, $params, $startDate, $endDate) {
+        $result = Cache::remember($this->cacheKey($metrics, $params), 0, function() use ($metrics, $params) {
             $response = $this->analytics->data_ga->get(
                 'ga:'.$this->viewId,
-                $startDate,
-                $endDate,
+                $this->startDate,
+                $this->endDate,
                 $metrics,
                 $params
             );
@@ -118,5 +134,50 @@ class Analytics
     public function timezone($timezone)
     {
         $this->timezone = $timezone;
+        return $this;
+    }
+
+
+    /**
+     * Calculate the time till the end of the day
+     * @return [type] [description]
+     */
+    private function calculateSecondsTillEndOfDayWithUsersTimezone()
+    {
+        // get user timezone
+        // $timezone = DateTimeZone::listIdentifiers(DateTimeZone::ALL)[Cms::user()->timezone];
+        // calculate seconds till end of day in the timezone of the user
+        return Carbon::now($this->timezone)->endOfDay()->diffInSeconds(Carbon::now($this->timezone));
+    }
+
+    /**
+     * Create a cache key
+     * @param  string $metrics
+     * @param  mixed $params
+     * @return string
+     */
+    private function cacheKey($metrics, $params)
+    {
+        $variables = [$this->timezone, $this->startDate, $this->endDate, $this->viewId, $params, $metrics];
+        return serialize($variables);
+    }
+
+    /**
+     * Reset the main functionality after a run
+     * @return class this
+     */
+    private function reset()
+    {
+        $this->days(7);
+        return $this;
+    }
+
+    /**
+     * Make it easy to extend with your own functions on this class
+     * @return class this
+     */
+    public function extend()
+    {
+        return $this;
     }
 }
